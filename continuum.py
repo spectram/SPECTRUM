503b1378fa9dc73b514d9467f14b1177a093514b
Archie-41 <154491127+Archie-41@users.noreply.github.com>
2023-12-21 10:29:51 +0530

    Add files via upload

---
+++ continuum.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.units as u
+from mpdaf.obj import Cube, WCS, WaveCoord
+#WCS for spatial coordinates and WaveCoord for spectral coordinates
+
+obj1 = Cube('DATA/ADP.2017-03-23T15_47_52.027.fits')
+obj1.info()
+
+from mpdaf.obj import iter_spe
+from tqdm import tqdm
+
+def continuum_extraction(cube,cont_cube):
+    for sp, co in tqdm(zip(iter_spe(cube), iter_spe(cont_cube))):        
+        co[:] = sp.poly_spec(1)
+
+#duplicate obj1
+
+small = obj1[:,0:10,0:10] #specify dimensions as needed
+small.info()
+
+cont_cube = small.clone(data_init=np.empty, var_init=np.zeros)
+cont_cube.info()
+
+
+continuum_extraction(small,cont_cube)
